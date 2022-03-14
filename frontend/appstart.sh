37461048a9497e8041be5dd38fa43d037f7bd028
Bankhead <esec26@gmail.com>
2022-03-14 22:31:20 +0000

    starting front end

---
+++ frontend/appstart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+
+#install  curl
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
+#install nginx
+sudo apt-get install nginx -y
