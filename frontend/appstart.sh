3ce0771974f5ae3ecebec0829e3af279dfd78b35
HASSAN1A <okothhassanjuma@gmail.com>
2022-03-15 18:51:11 +0300

    Add  node installation and cloning the repo sitory

--- frontend/appstart.sh
+++ frontend/appstart.sh
@@ -7,3 +7,11 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
 #install nginx
 sudo apt-get install nginx -y
+
+#install node.js
+sudo apt-get install -y nodejs
+
+cd /var/www/html/
+
+#clone repo
+sudo git clone https://github.com/sdg000/Mobalysis.git
