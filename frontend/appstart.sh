d3dd7cc97680cb45d211a44ef1034826793a907f
Rove-Sudo <veronmbuthia@gmail.com>
2022-03-15 21:36:28 +0300

    adding to appstart.sh

--- frontend/appstart.sh
+++ frontend/appstart.sh
@@ -15,3 +15,17 @@ cd /var/www/html/
 
 #clone repo
 sudo git clone https://github.com/sdg000/Mobalysis.git
+
+#cd into frontend to install and run npm
+cd Mobalysis/frontend
+sudo npm install
+sudo npm run build
+
+#copy all files in  frontend/build to /var/www/html
+#remove original html file from nginx
+sudo cp -r /var/www/html/Mobalysis/frontend/build/* /var/www/html
+sudo rm -rf index.nginx.debian.html
+
+#restart nginx
+sudo systemctl restart nginx
+
